242657c4f07ea675cc7cfea8618f78f5e9fd8f0d
olga <olgaconesaboj@gmail.com>
2020-06-13 14:00:19 +0200

    Tipo de datos

---
+++ 02-variables-y-tipos/tipos.py
@@ -0,0 +1,42 @@
+# Tipo de dato None (significa que no tiene valor la variable)
+nada = None
+
+# Tipo de dato cadena
+cadena = "Hola soy Olga Conesa"
+cadena = "Programadora Web"
+
+# Tipo de dato entero(numero sin decimales)
+entero = 99
+
+# Tipo de dato float(decimal)
+flotante = 14.25
+
+
+# Tipo de dato booleano(indica true o false)
+booleano = True
+booleano = False
+
+# Tipo de dato lista(array)
+lista = [10,20,30,100]
+listaString = [44, "treinta", 30, "cuarenta"]
+
+# Tipo de dato tupla(lista de datos que no canvian)
+tupla = ("master", "en", "python")
+
+# Tipo de dato diccionario(coleccion de datos de clave y valor)
+diccionario = {
+    "nombre": "Olga",
+    "apellido": "Conesa",
+    "curso": "Master en Python"
+}
+
+# Tipo de dato rango
+rango = range(9)
+
+
+#Impresion Varibales
+print(dato_byte)
+
+
+#Mostrat tipo de dato
+print(type(dato_byte))
