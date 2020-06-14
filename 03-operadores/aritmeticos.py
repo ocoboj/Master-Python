6bfc3eba05aa8f0b1cd65d5b956128f5701b044c
olga <olgaconesaboj@gmail.com>
2020-06-14 10:49:54 +0200

    Operadores Aritmeticos

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,17 @@
+# Operadores Aritmeticos
+# El uso del signo igual "=", es un operador de asiganción
+numero1 = 77
+numero2 = 44
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+resto = numero1 % numero2 #Lo que sobre de la división
+
+print("********CALCULADORA********")
+
+print(f"La resta es: {resta}")
+print(f"La suma es: {numero1 + numero2}")
+print("La multiplicacion es: ", multiplicacion)
+print("La division es: ", division)
+print("El resto de la division es: ", resto)
