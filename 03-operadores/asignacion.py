7dfbc9da094810dc24fcba1d589530b27708adfb
olga <olgaconesaboj@gmail.com>
2020-06-14 11:01:40 +0200

    Operadores de Asigancion, Incremento y Decremento

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,33 @@
+# Operadores de Asigancion (permite dar valor a una variable)
+
+edad = 39
+
+print(edad)
+
+# suma 5 al valor de edad que teniamos al principio es decir los 39
+edad += 5 
+# seria lo mimso que poner
+# edad = edad + 5
+edad -= 5
+print(edad)
+
+# Operadores de incremento y decremento
+year = 2021
+
+# Incremento
+# year = year +1
+year +=1
+print(year)
+
+# Decremento
+# year = year -1
+year -= 1
+print(year)
+
+# Pre incremento
+year = 1 + year
+print(year)
+
+# Pre decremento
+year = 1 - year
+print(year)
