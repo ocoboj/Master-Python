9f80322faa5c2d361f9e407ef010ed8000d28106
olga <olgaconesaboj@gmail.com>
2020-07-19 11:27:00 +0200

    Modulos matemáticos

---
+++ 12-modulos/modulomath.py
@@ -0,0 +1,20 @@
+# Modulo matematicas
+import math
+
+# sacar raiz cuadrada de un numero
+print("Raiz cuadrada de 10: ", math.sqrt(10))
+
+# Numero pi
+print("Numero pi: ", float(math.pi))
+
+# Redondeo a la alta
+print("Redondear a la alta: ", math.ceil(6.56789))
+
+# Redondeo a la baja
+print("Redondear a la baja: ", math.floor(6.56789))
+
+# Modulo random
+import random
+
+print("Numero aleatorio entre 15 y 67: ", random.randint(15,67))
+
