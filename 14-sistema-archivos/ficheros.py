d8fbca4f939e69535bb334bef1a680f695b57d7d
olga <olgaconesaboj@gmail.com>
2020-07-19 13:01:42 +0200

    copiar ficheros

--- 14-sistema-archivos/ficheros.py
+++ 14-sistema-archivos/ficheros.py
@@ -12,3 +12,23 @@ archivo.write("******* Soy un texto metido desde python *******\n")
 
 # Cerrar archivo
 archivo.close()
+
+# Abrir archivo
+ruta = str(pathlib.Path().absolute()) + "/fichero_texto.txt"
+archivo_lectura = open(ruta, "r")
+
+# Leer contenido
+""" contenido = archivo_lectura.read()
+print(contenido) """
+
+# Leer contenido y guardarlo en una lista
+lista = archivo_lectura.readlines()
+archivo_lectura.close()
+
+print(lista)
+
+for frase in lista:
+    """ lista_frase = frase.split()
+    print(lista_frase) """
+    if not frase.isnumeric():
+        print(frase.center(100))
